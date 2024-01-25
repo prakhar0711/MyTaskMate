94d64ff93d4cc9c035b1d23bdc41cb60a1b62cdd
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-01-25 22:59:23 +0530

    vercel,setting commit

--- multiuserToDoApp/settings.py
+++ multiuserToDoApp/settings.py
@@ -116,6 +116,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = BASE_DIR / "static",
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
