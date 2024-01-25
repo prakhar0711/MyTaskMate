bedcca6e7aa36d819f84dac6e5ae3f62e78b051c
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-01-25 22:54:50 +0530

    vercel,setting commit

--- multiuserToDoApp/settings.py
+++ multiuserToDoApp/settings.py
@@ -117,7 +117,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = [BASE_DIR / 'staticfiles_build', 'static ']
